fdeda4de23f0c93eeddd1df47e6106474572431f
Dave_Cray <9052299+DaveCray86@users.noreply.github.com>
2019-04-17 16:00:34 -0700

    Insurance approval project
    Insurance approval project

---
+++ Insurance Approval/Insurance Approval/Program.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insurance_Approval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            //string answer;
+            Console.WriteLine("Have you ever had a DUI charge? Enter True or False: ");
+            string answer = Console.ReadLine().ToLower();
+            bool answer1 = Convert.ToBoolean(answer);
+
+            
+            Console.WriteLine("How many speeding tickets do you have?: ");
+            int speedTick = Convert.ToInt32(Console.ReadLine());
+
+            if (age >= 15 && answer1 == false && speedTick <= 3)
+            {
+                Console.WriteLine("You are qualified.");
+                Console.ReadLine();
+            }
+            else if (age >= 15 && answer1 == true && speedTick <= 3)
+            {
+                Console.WriteLine("You are not qualified.");
+                Console.ReadLine();
+            }
+            else if (age < 15 || answer1 == false || speedTick > 3)
+            {
+                Console.WriteLine("You are not qualified.");
+                Console.ReadLine();
+            }
+            else if (age < 15 || answer1 == true || speedTick > 3)
+            {
+                Console.WriteLine("You are not qualifed.");
+                Console.ReadLine();
+            }
+            else if (age >= 15 && answer1 == false && speedTick < 3)
+            {
+                Console.WriteLine("You are not qualified.");
+                Console.ReadLine();
+            }
+            
+
+        }
+    }
+}
