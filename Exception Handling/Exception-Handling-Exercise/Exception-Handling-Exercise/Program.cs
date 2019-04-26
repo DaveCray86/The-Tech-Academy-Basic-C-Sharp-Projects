0c0f3b6c9150cc20bd6f33bd8381a6436b55629b
Dave_Cray <9052299+DaveCray86@users.noreply.github.com>
2019-04-25 23:10:05 -0700

    Updated to work
    Updated code to work properly.

--- Exception Handling/Exception-Handling-Exercise/Exception-Handling-Exercise/Program.cs
+++ Exception Handling/Exception-Handling-Exercise/Exception-Handling-Exercise/Program.cs
@@ -12,21 +12,42 @@ namespace Exception_Handling_Exercise
         {
             //1. Create a list of integers. Ask the user for a number to divide each number in the list by. 
             //Write a loop that takes each integer in the list, divides it by the number the user entered, and displays the result to the screen.
-            List<int> testScores = new List<int>() { 85, 68, 99, 88, 75, 78, 84, 73, 84, 97 };
 
-            Console.WriteLine("I have a list of numbers. Please enter a number to divide my numbers by: ");
-            int div1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("You picked " + div1);
-            Console.ReadLine();
-            while (guess != secWord)
+            try
             {
-                Console.WriteLine("Enter a guess: ");
-                guess = Console.ReadLine();
+                List<int> testScores = new List<int>();
+                testScores.Add(85);
+                testScores.Add(68);
+                testScores.Add(99);
+                testScores.Add(88);
+                testScores.Add(75);
+                testScores.Add(78);
+                testScores.Add(84);
+                testScores.Add(73);
+                testScores.Add(97);
+
+                Console.WriteLine("Please enter a number to divide by: ");
+                int input = Convert.ToInt32(Console.ReadLine());
+
+                foreach (int score in testScores)
+                {
+                    Console.WriteLine(score / input);
+                }
             }
-            Console.WriteLine("You Win!");
-            Console.ReadLine();
-
-
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Remember you cannot divide by zero.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            
 
             //2. Run that code, entering in non-zero numbers as the user. Look at the displayed results.
 
@@ -34,6 +55,7 @@ namespace Exception_Handling_Exercise
 
             //3. Run that code, entering in zero as the number to divide by. Note any error messages you get.
 
+            //System cannot divide by 0.
 
 
             //4. Run that code, entering in a string as the number to divide by. Note any error messages you get.
@@ -45,15 +67,7 @@ namespace Exception_Handling_Exercise
             //In the catch block, display the error message to the screen. Then try various combinations of user input: valid numbers, zero and a string. 
             //Ensure the proper error messages display on the screen, and that the code after the try/catch block gets executed.
 
-            try
-            {
-                catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                return;
-            }
-            Console.ReadLine();
-            }
-        }
+            
+        }   
     }
 }
