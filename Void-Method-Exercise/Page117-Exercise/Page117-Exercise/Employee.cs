f2c387fbf02ae535159eeeb4b7437e5dd0715b20
Dave_Cray <9052299+DaveCray86@users.noreply.github.com>
2019-05-09 12:56:53 -0700

    Page 117 Exercise.
    Page 117 Exercise.

---
+++ Void-Method-Exercise/Page117-Exercise/Page117-Exercise/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page117_Exercise
+{
+    public class Employee : Person                      //Pulled from Person class.
+    {
+        public int callID { get; set; }
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + fName + " " + lName);
+        }
+    }
+}
