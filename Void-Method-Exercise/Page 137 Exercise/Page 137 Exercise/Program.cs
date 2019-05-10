abd73402737f312fae484c6b5b1922b4de7c63a1
Dave_Cray <9052299+DaveCray86@users.noreply.github.com>
2019-05-10 14:53:10 -0700

    Page 137 Exercise.
    Page 137 Exercise.

---
+++ Void-Method-Exercise/Page 137 Exercise/Page 137 Exercise/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page_137_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Number number = new Number();
+            number.Amount = 44;
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
