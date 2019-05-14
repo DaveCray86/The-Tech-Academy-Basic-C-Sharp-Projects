38ed47fb0d549328d16549ae942782f317336860
Dave_Cray <9052299+DaveCray86@users.noreply.github.com>
2019-05-14 09:20:22 -0700

    Page 162 Exercise.
    Page 162 Exercise.

---
+++ Void-Method-Exercise/Page 162 Exercise/Page 162 Exercise/Page 162 Exercise/Name1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page_162_Exercise
+{
+    class Name1
+    {
+        string _fname = "";
+        string _id = "";
+
+        public Name1(string name, string id)
+        {
+            _fname = name;
+            _id = id;
+            Console.WriteLine("Employee name and id is: {0}, {1}", name, id);
+        }
+
+        public Name1(string name) : this(name, "5236")
+        {
+        }
+    }
+}
---
+++ Void-Method-Exercise/Page 162 Exercise/Page 162 Exercise/Page 162 Exercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page_162_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //const variable
+            const string name = "Jon";
+            Console.WriteLine("Hi, my name is {0}", name);
+
+            //using 'var'
+            var names = new List<string>() { "Jon", "Cole", "Steve", "Iren" };
+            Console.WriteLine(names[0]);
+
+            //chaining constructors
+            Name1 x = new Name1("Jon");
+
+            Console.ReadLine();
+        }
+    }
+}
