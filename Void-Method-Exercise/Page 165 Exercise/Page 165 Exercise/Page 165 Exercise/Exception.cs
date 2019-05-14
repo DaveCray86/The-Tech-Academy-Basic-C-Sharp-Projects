2f49c5b991996b665274e0770a3e373514dbea4e
Dave_Cray <9052299+DaveCray86@users.noreply.github.com>
2019-05-14 10:39:16 -0700

    Page 165 Exercise.
    Page 165 Exercise.

---
+++ Void-Method-Exercise/Page 165 Exercise/Page 165 Exercise/Page 165 Exercise/Exception.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page_165_Exercise
+{
+    public class FormatException : Exception
+    {
+        public FormatException()
+        {
+            Console.WriteLine("That is not a valid age.");
+            Console.ReadLine();
+            return;
+        }
+    }
+
+    public class ZeroException : Exception
+    {
+        public ZeroException()
+        {
+            Console.WriteLine("You cannot input a 0 for your age.");
+            return;
+        }
+    }
+
+    public class NegException : Exception
+    {
+        public NegException()
+        {
+            Console.WriteLine("You cannot enter a negative numebr as an age.");
+            Console.ReadLine();
+            return;
+        }
+    }
+}
---
+++ Void-Method-Exercise/Page 165 Exercise/Page 165 Exercise/Page 165 Exercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page_165_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter your age: ");
+                int age = Convert.ToInt32(Console.ReadLine());
+                if (age == 0) throw new ZeroException();
+                if (age < 0) throw new NegException();
+
+                Console.WriteLine("You were born in year {0}.", 2019 - age);
+                Console.ReadLine();
+            }
+            catch (ZeroException) { }
+            catch (NegException) { }
+            catch (Exception other)
+            {
+                Console.WriteLine("That is not a valid age.", other);
+                Console.ReadLine();
+                return;
+            }
+        }
+    }
+}
