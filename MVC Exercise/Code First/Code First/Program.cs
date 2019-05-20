22e52389927490ce5d274d867416b4e108d4401e
Dave_Cray <9052299+DaveCray86@users.noreply.github.com>
2019-05-19 17:06:36 -0700

    Code First MVC Project.
    Code First MVC Project.

---
+++ MVC Exercise/Code First/Code First/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new GroceryCOntext())
+            {
+                Console.WriteLine("Do you have an item to add to your grocery list?");
+                string answer = Console.ReadLine();
+                if (answer.ToLower() == "yes" || answer.ToLower() == "y")
+                {
+                    Console.WriteLine("Please enter an item for your grocery list: ");
+                    var item = Console.ReadLine();
+
+                    //Save to DB
+                    var listItem = new Groceries { Item = item };
+                    db.groceryList.Add(listItem);
+                    db.SaveChanges();
+
+                    Console.WriteLine("Do you want to have your list displayed?");
+                    string reply = Console.ReadLine();
+                    if (reply.ToLower() == "yes" || reply.ToLower() == "y")
+                    {
+                        // Display items from the database
+                        var query = from b in db.groceryList
+                                    orderby b.Item
+                                    select b;
+                        Console.WriteLine("All items on your list: ");
+                        foreach (var x in query)
+                        {
+                            Console.WriteLine(x.Item);
+                        }
+                        Console.ReadLine();
+                        Console.WriteLine("Thank you for using your Console Grocry List!");
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Thank you for using your Console Grocery List!");
+                        Console.ReadLine();
+                    }
+                }
+                else if (answer.ToLower() == "no" || answer.ToLower() == "n")
+                {
+                    Console.WriteLine("Thank you for using your Console Grocery List!");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+
+    public class Groceries
+    {
+        public int Id { get; set; }
+        public string Item { get; set; }
+
+        public virtual List<Groceries> groceryList { get; set; }
+    }
+
+    public class GroceryCOntext : DbContext
+    {
+        public DbSet<Groceries> groceryList { get; set; }
+    }
+}
--- MVC Exercise/Code First/Code First/Properties/AssemblyInfo.cs
+++ MVC Exercise/Code First/Code First/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BasicAutoInsurance")]
+[assembly: AssemblyTitle("Code First")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BasicAutoInsurance")]
+[assembly: AssemblyProduct("Code First")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fe536b35-204a-4b0d-9861-13c2bc557dcf")]
+[assembly: Guid("fb5db6fd-db15-4a2e-84c3-497ca82679b5")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
