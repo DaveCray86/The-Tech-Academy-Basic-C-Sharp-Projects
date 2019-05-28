56afbe8ac8297c2fd18153e8cf1150e6b76a09b7
Dave_Cray <9052299+DaveCray86@users.noreply.github.com>
2019-05-28 10:25:21 -0700

    Contoso University project.
    Contoso University project.

---
+++ Contoso1/ContosoUniversity/ContosoUniversity/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        //StringLength, won't let a user enter white space for a name.
+        //MaxLength can be used same as StringLength, but does not provide client side validation.
+
+        public int ID { get; set; }
+        [StringLength(50)]
+        public string LastName { get; set; }
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [Column("FirstName")]
+        public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
